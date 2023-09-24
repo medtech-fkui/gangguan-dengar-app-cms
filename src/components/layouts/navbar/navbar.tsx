7088dabf6a3be12cac5833b89502c1b9b85c177c
AnggaPutraa <yananggaa@gmail.com>
2023-09-24 22:27:45 +0700

    refactor(navbar): adjust redirect on logo click

--- src/components/layouts/navbar/navbar.tsx
+++ src/components/layouts/navbar/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Wrapper } from "..";
 import { NavbarMenuItems } from "./navbar-menu-items";
 import { NavbarUserMenu } from "./navbar-user-menu";
@@ -9,9 +10,11 @@ export const Navbar = () => {
         <Wrapper>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <div className="flex flex-cols gap-x-12">
-              <h2 className="font-medium cursor-pointer">
-                Telinga Kecilku CMS
-              </h2>
+              <Link href="/users">
+                <h2 className="font-medium cursor-pointer">
+                  Telinga Kecilku CMS
+                </h2>
+              </Link>
               <NavbarMenuItems />
             </div>
             <NavbarUserMenu />
